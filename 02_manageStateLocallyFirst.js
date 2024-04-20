885d4abcbb61bb640b89f19d5166b68f8ca10839
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-04-20 21:42:01 +0300

    Map the messages array to list item elements  and render them

--- 02_manageStateLocallyFirst.js
+++ 02_manageStateLocallyFirst.js
@@ -10,13 +10,14 @@ class DisplayMessages extends React.Component {
     // Add handleChange() and submitMessage() methods here
 
     render() {
+        const allMessages = this.state.messages.map(message => <li>{message}</li>);
         return (
         <div>
             <h2>Type in a new Message:</h2>
             { /* Render an input, button, and ul below this line*/ }
             <input onChange={this.handleChange}>{this.input}</input>
             <button></button>
-            <ul><li></li></ul>
+            <ul>{allMessages}</ul>
             { /* Change code above this line */ }
         </div>
         );
