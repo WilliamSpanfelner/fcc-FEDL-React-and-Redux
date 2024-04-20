e92a0a605d46987460f1957548076179188a783f
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-04-20 23:05:19 +0300

    Add submitMessage method and bind it

--- 02_manageStateLocallyFirst.js
+++ 02_manageStateLocallyFirst.js
@@ -7,6 +7,7 @@ class DisplayMessages extends React.Component {
             messages: []
         }
         this.handleChange = this.handleChange.bind(this);
+        this.submitMessage = this.submitMessage.bind(this);
     }
     // Add handleChange() and submitMessage() methods here
     handleChange(e) {
@@ -18,6 +19,17 @@ class DisplayMessages extends React.Component {
         });
     }
 
+    submitMessage() {
+        const newMessage = this.state.input;
+        const allMessages = this.state.messages.concat(newMessage);
+        this.setState (() => {
+            return {
+                input: '',
+                messages: allMessages
+            };
+        });
+    }
+
     render() {
         const allMessages = this.state.messages.map(message => <li>{message}</li>);
         return (
@@ -25,7 +37,7 @@ class DisplayMessages extends React.Component {
             <h2>Type in a new Message:</h2>
             { /* Render an input, button, and ul below this line*/ }
             <input onChange={this.handleChange} value={this.state.input} />
-            <button></button>
+            <button onClick={this.submitMessage} />
             <ul>{allMessages}</ul>
             { /* Change code above this line */ }
         </div>
