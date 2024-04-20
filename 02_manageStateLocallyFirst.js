557020cc3360740abaf5f059e564c1c5fe5dd44d
William Spanfelner <onesmallstepforman.apollo11@gmail.com>
2024-04-20 22:26:23 +0300

    Create handleChange method and bind it

--- 02_manageStateLocallyFirst.js
+++ 02_manageStateLocallyFirst.js
@@ -6,8 +6,17 @@ class DisplayMessages extends React.Component {
             input: "",
             messages: []
         }
+        this.handleChange = this.handleChange.bind(this);
     }
     // Add handleChange() and submitMessage() methods here
+    handleChange() {
+        const newMessage = document.querySelector("input").value;
+        this.setState (() => {
+            return {
+                input: newMessage
+            };
+        });
+    }
 
     render() {
         const allMessages = this.state.messages.map(message => <li>{message}</li>);
@@ -15,7 +24,7 @@ class DisplayMessages extends React.Component {
         <div>
             <h2>Type in a new Message:</h2>
             { /* Render an input, button, and ul below this line*/ }
-            <input onChange={this.handleChange}>{this.input}</input>
+            <input onChange={this.handleChange} value={this.state.input} />
             <button></button>
             <ul>{allMessages}</ul>
             { /* Change code above this line */ }
